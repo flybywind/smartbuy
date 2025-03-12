1db804f51d4601f698189bf65668cdc0eb03cba3
tongxin.wen <tongxinwen@fintopia.tech>
2025-03-13 07:16:03 +0800

    init expo project

--- app/metro.config.js
+++
@@ -1,9 +0,0 @@
-const { getDefaultConfig } = require('expo/metro-config');
-
-const defaultConfig = getDefaultConfig(__dirname);
-
-// 添加额外的忽略模式
-defaultConfig.watchFolders = [__dirname];
-defaultConfig.resolver.blockList = [/node_modules\/.*/];
-
-module.exports = defaultConfig; 
--- app/src/utils/images.ts
+++
@@ -1,5 +0,0 @@
-// 由于React Native在处理SVG文件时的限制，我们可以创建一个辅助函数来处理图像资源的导入
-import { ImageSourcePropType } from "react-native";
-export const images = { logo: require("../assets/logo.svg") as ImageSourcePropType };
-export const getImage = (name: keyof typeof images): ImageSourcePropType => { return images[name]; };
-export const svgToPng = (svgSource: any): ImageSourcePropType => { return svgSource as ImageSourcePropType; };
